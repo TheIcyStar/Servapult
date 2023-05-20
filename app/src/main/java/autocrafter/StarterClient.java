9617719dc57c1a9b67a55649db6c3024fabce439
Potenchill <tsunamitacoman@gmail.com>
2023-05-20 18:26:05 -0500

    starting

--- app/src/main/java/autocrafter/StarterClient.java
+++ app/src/main/java/autocrafter/StarterClient.java
@@ -1,5 +1,14 @@
 package autocrafter;
 
+// import io.github.cdimascio.dotenv.Dotenv;
+
 public class StarterClient {
     
+    public void start() {
+        System.out.println("starting!!!");
+
+        // Dotenv dotenv = Dotenv.load();
+        // String token = dotenv.get("DISCORD_BOT_TOKEN");
+    }
+
 }
