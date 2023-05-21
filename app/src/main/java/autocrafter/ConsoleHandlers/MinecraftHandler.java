e5f7db78a0298518c1675c9038aac28fd8f8b674
TheIcyStar <theicystar@users.noreply.github.com>
2023-05-20 22:59:45 -0400

    general fixes

--- app/src/main/java/autocrafter/ConsoleHandlers/MinecraftHandler.java
+++ app/src/main/java/autocrafter/ConsoleHandlers/MinecraftHandler.java
@@ -1,8 +1,21 @@
 package autocrafter.ConsoleHandlers;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import autocrafter.Models.ConsoleHandler;
 
 public class MinecraftHandler extends ConsoleHandler {
     public void handleLine(String line){
         System.out.println("handled:"+line);
+
+        //todo: fix output to file
+        File file = new File("lastrun.log");
+        try(FileWriter fr = new FileWriter(file, true)){
+            fr.write("data");
+
+        } catch(IOException e){
+            System.out.println();
+        }        
     }
 }
--- app/src/main/java/autocrafter/HosterClient.java
+++ app/src/main/java/autocrafter/HosterClient.java
@@ -5,11 +5,10 @@
 package autocrafter;
 
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.File;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.ConsoleHandler;
 
 import autocrafter.ConsoleHandlers.MinecraftHandler;
 
@@ -28,7 +27,7 @@ public class HosterClient {
 
     public HosterClient(String path, String[] args){
         ArrayList<String> list = new ArrayList<>();
-        list.add("java");
+        list.add("C:\\Program Files\\Java\\jdk-17.0.2\\bin\\javaw.exe");
         for(String arg : args){
             list.add(arg);
         }
@@ -36,12 +35,15 @@ public class HosterClient {
         list.add("nogui");
 
         processBuilder = new ProcessBuilder(list);
+        processBuilder.directory(new File("Q:\\Servers\\DummyMC"));
     }
 
     public void start(){
         try{
             process = processBuilder.start();
             BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            BufferedReader stdErr = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
             MinecraftHandler handler = new MinecraftHandler();
 
             String line;
@@ -54,6 +56,12 @@ public class HosterClient {
                 System.out.println(line);
 
                 TimeUnit.MILLISECONDS.sleep(200);
+            }
+
+            String errLine = stdErr.readLine();
+            while(errLine != null){
+                System.out.println(errLine);
+                line = stdErr.readLine();
             }
 
             System.out.println("Done");
