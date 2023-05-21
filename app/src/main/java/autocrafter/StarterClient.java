1939ab0d24dfef4a35d95d5b837dfa39f0d79ef0
Potenchill <tsunamitacoman@gmail.com>
2023-05-20 19:20:37 -0500

    the bot lives

--- app/src/main/java/autocrafter/StarterClient.java
+++ app/src/main/java/autocrafter/StarterClient.java
@@ -1,14 +1,20 @@
 package autocrafter;
 
-// import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
 
 public class StarterClient {
     
     public void start() {
-        System.out.println("starting!!!");
 
-        // Dotenv dotenv = Dotenv.load();
-        // String token = dotenv.get("DISCORD_BOT_TOKEN");
+        Dotenv dotenv = Dotenv.load();
+        String token = dotenv.get("DISCORD_BOT_TOKEN");
+
+
+        JDA jda = JDABuilder.createDefault(token).build();
+
+        
     }
 
 }
