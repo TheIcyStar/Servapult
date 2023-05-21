70312d93f3d9845c5d30f5127387eb6868a5650d
Potenchill <tsunamitacoman@gmail.com>
2023-05-20 21:58:57 -0500

    Removed unused imports

--- app/src/main/java/autocrafter/StarterClient.java
+++ app/src/main/java/autocrafter/StarterClient.java
@@ -4,8 +4,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.interactions.commands.SlashCommandReference;
-import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 
 import java.util.ArrayList;
