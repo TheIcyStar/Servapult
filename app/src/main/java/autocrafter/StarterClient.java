788a8434faabd0f1912ac2b0540929c59a1f839a
Potenchill <tsunamitacoman@gmail.com>
2023-07-18 01:14:51 -0500

    Tests .env variables

--- app/src/main/java/autocrafter/StarterClient.java
+++ app/src/main/java/autocrafter/StarterClient.java
@@ -14,9 +14,20 @@ public class StarterClient {
     
     public void start() {
 
+        // Load relevant .env variables
         Dotenv dotenv = Dotenv.load();
         String token = dotenv.get("DISCORD_BOT_TOKEN");
+        // Test token
+        if (token == null) {
+            System.err.println("Token in .env is null!");
+            return;
+        }
         String guildId = dotenv.get("DISCORD_SERVER_ID");
+        // Test guild ID
+        if (guildId == null) {
+            System.err.println("Guild ID in .env is null!");
+            return;
+        }
 
         // Start up the bot
         JDA jda = JDABuilder.createDefault(token).build();
@@ -29,6 +40,11 @@ public class StarterClient {
 
         System.out.println("Guild ID: " + guildId);
         Guild guild = jda.getGuildById(guildId);
+        // Test guild ID again to see if it exists
+        if (guild == null ) {
+            System.err.println("Guild with ID " + guildId + " does not exist!");
+            return;
+        }
 
         // Load commands
         ArrayList<SlashCommandData> slashCommands = new ArrayList<SlashCommandData>();
